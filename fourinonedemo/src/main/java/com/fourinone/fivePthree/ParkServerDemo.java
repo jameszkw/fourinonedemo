2d79dabf69dc0efd90b703edb0a98466b4048155
jameszkw <675567108@qq.com>
2015-09-22 21:26:58 +0800

    first commit

---
+++ fourinonedemo/src/main/java/com/fourinone/fivePthree/ParkServerDemo.java
@@ -0,0 +1,10 @@
+package com.fourinone.fivePthree;
+import com.fourinone.BeanContext;
+
+public class ParkServerDemo
+{
+	public static void main(String[] args)
+	{
+		BeanContext.startPark();
+	}
+}
